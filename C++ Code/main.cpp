e5189f3282c090abc498428b0f641690792d6398
kshitijmall <horizonkind@gmail.com>
2019-02-18 15:32:43 -0500

    Commented the code and made it more generalized

--- C++ Code/main.cpp
+++ C++ Code/main.cpp
@@ -1,20 +1,18 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
+// To use mathematical quantities like PI we need the following two lines
+#define _USE_MATH_DEFINES
 #include <math.h>
-#include <time.h>
-//#include <cmath>
-using namespace std;
-using namespace std::chrono;
+#include <time.h> // This header file is needed to calculate computation time
 
-const int N = 1000; // time steps
-const double g = 9.80665;
-const double re = 6378000.0;
-const double B = 157.0;
-const double rho0 = 1.225;
-const double H = 7200.0;
+using namespace std; // This helps to avoid writing std:: at several places
 
+// Define the constants used for the hypersonic problem
+const int N = 1000; // Time steps
+const int S = 3; // Number of states
+
+// Declare the functions used inside main
 double * eom(double t, double x[]);
-double ** rk4(double a, double b, int N, double alpha[]);
+double ** rk4(double a, double b, double alpha[]);
 void printResult(double ** result);
 
 //******************************************************************************
@@ -23,117 +21,215 @@ int main (int argc, const char * argv[]) {
 
 //******************************************************************************
 //
-//  Purpose:
-//
-//    MAIN is the main program for RK4.
-//
-//  Discussion:
-//
-//    RK4_PRB tests the RK4 library.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license.
-//
-//  Modified:
-//
-//    17 February 2019
-//
 //  Author:
 //
 //    Kshitij Mall
 //
+//  Modified:
+//
+//    18 February 2019
+//
+//  Purpose:
+//
+//    main is the main program for this project.
+//
+//  Inputs:
+//
+//    argc: Number of command line arguments
+//    argv: value of array of command line arguments
+//
+//  Output:
+//
+//    Returns 0
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license
+//
+//******************************************************************************
 cout << "\n" << "C++ version\n";
-cout << "Test the RK4 function.\n";
 
 const double vatm = 11060.0; // Entry Velocity, m/s
 const double hatm = 80000.0; // Entry Height, m
 const double gamma0 = -50.0/180.0*M_PI; // Initial flight path angle, rad
 
-const double t0 = 0.0; // initial time
-const double tf = 206.0; // final time
+const double t0 = 0.0; // Initial time
+const double tf = 212.2; // Final time
 
 double init[] = {hatm,vatm,gamma0}; // Initial guess
 
 // Get starting timepoint
 clock_t t;
-	t = clock();
-double ** result = rk4(t0, tf, N, init);
+t = clock();
+
+// Call the RK4 function to obtain the ODE solution
+double ** result = rk4(t0, tf, init);
 
 // Get ending timepoint
 t = clock() - t;
-	cout << "Computation Time: " << t*1.0/CLOCKS_PER_SEC << " seconds" << endl;
 
-//printResult(result);
+// Display the computation time
+cout << "Computation Time: " << t*1.0/CLOCKS_PER_SEC << " seconds" << endl;
+
+//printResult(result); // Print the state vector
 return 0;
 }
 
-// Function for equations of motion
+//******************************************************************************
+
 double * eom(double t, double x[]) {
 
-    static double xdot[3];
+//******************************************************************************
+//
+//  Author:
+//
+//    Kshitij Mall
+//
+//  Modified:
+//
+//    18 February 2019
+//
+//  Purpose:
+//
+//    eom is the function file to obtain the derivative vector at a given time.
+//
+//  Inputs:
+//
+//    t: Time at which derivative needs to be evaluated
+//    x[]: starting state values for h, v, and fpa at the input time
+//
+//  Output:
+//
+//    xdot: Vector output with derivatives for each of the three input states
+//
+//******************************************************************************
+	const double g = 9.80665; // Acceleration due to gravity, m/s^2
+	const double re = 6378000.0; // Radius of Earth, m
+	const double B = 157.0; // Ballistic coefficient, kg/m^2
+	const double rho0 = 1.225; // Surface atmospheric density, kg/m^3
+	const double H = 7200.0; // Scale height, m
 
-    xdot[0] = x[1]*sin(x[2]);
-    xdot[1] = -rho0*exp(-x[0]/H)*pow(x[1],2.0)/(2.0*B) - g*sin(x[2]);
-    xdot[2] = (x[1]/(re+x[0]) - g/x[1])*cos(x[2]);
+  static double xdot[S]; // Declare the derivative vector
 
-    return xdot;
+  xdot[0] = x[1]*sin(x[2]); // hdot
+  xdot[1] = -rho0*exp(-x[0]/H)*pow(x[1],2.0)/(2.0*B) - g*sin(x[2]); // vdot
+  xdot[2] = (x[1]/(re+x[0]) - g/x[1])*cos(x[2]); // fpa dot
+
+  return xdot;
 }
 
-double ** rk4(double a, double b, int N, double alpha[]){
+//******************************************************************************
 
-  double ** state = new double*[N];
+double ** rk4(double a, double b, double alpha[]){
+
+//******************************************************************************
+//
+//  Author:
+//
+//    Kshitij Mall
+//
+//  Modified:
+//
+//    18 February 2019
+//
+//  Purpose:
+//
+//    rk4 is the function file to obtain the state vector at all input times.
+//    A 4th order Runge Kutta method is used in this file.
+//
+//  Inputs:
+//
+//    a: Initial time
+//		b: Final time
+//    alpha[]: Initial state vector
+//
+//  Output:
+//
+//    state: A matrix of the state outputs at different times
+//
+//******************************************************************************
+  double ** state = new double*[N]; // Create N pointers to the rows of state
 
   double h = double((b-a)/N); // The step size
-  double * k1, * k2, * k3, * k4, t[N], k2inp[3], k3inp[3], k4inp[3];
+	// Initialize useful variables for the Runge Kutta formula
+  double * k1, * k2, * k3, * k4, t[N], k2inp[S], k3inp[S], k4inp[S];
 
   for (int i = 0; i < N+1; ++i) {
-       state[i] = new double[N];
+       state[i] = new double[N]; // Insert columns of states for each time input
        if(i == 0){
+				 // Initialize the time and state arrays
          t[i] = a;
-         state[i][0] = alpha[0];
-         state[i][1] = alpha[1];
-         state[i][2] = alpha[2];
+				 for (int j = 0; j < S; ++j) {
+         state[i][j] = alpha[j];
+			 	}
        }
        else{
          // Determine k1
          k1 = eom(t[i-1], state[i-1]);
 
          // Determine k2
-         for (int j = 0; j < 3; ++j) {
-          k2inp[j]  = state[i-1][j]+0.5*k1[j];
+         for (int j = 0; j < S; ++j) {
+          k2inp[j]  = state[i-1][j] + 0.5*k1[j];
          }
          k2 = eom(t[i-1] + h/2.0, k2inp);
 
          // Determine k3
-         for (int j = 0; j < 3; ++j) {
-          k3inp[j]  = state[i-1][j]+0.5*k2[j];
+         for (int j = 0; j < S; ++j) {
+          k3inp[j]  = state[i-1][j] + 0.5*k2[j];
          }
          k3 = eom(t[i-1] + h/2.0, k3inp);
 
          // Determine k4
-         for (int j = 0; j < 3; ++j) {
-          k4inp[j]  = state[i-1][j]+k3[j];
+         for (int j = 0; j < S; ++j) {
+          k4inp[j]  = state[i-1][j] + k3[j];
          }
          k4 = eom(t[i-1] + h, k4inp);
 
-         // Determine new result
-         for (int j = 0; j < 3; ++j) {
+         // Determine the new state from k1, k2, k3, k4, and the old state
+         for (int j = 0; j < S; ++j) {
            state[i][j] = state[i-1][j] + h*(k1[j] + 2.0*k2[j] + 2.0*k3[j] + k4[j])/6.0;
          }
-         t[i] = t[i-1] + h;
+
+				 t[i] = t[i-1] + h; // Determine the new time
        }
   }
-  
-  return state;
+
+  return state; // Return the state vector
 }
 
+//******************************************************************************
+
 void printResult(double ** state){
+
+//******************************************************************************
+//
+//  Author:
+//
+//    Kshitij Mall
+//
+//  Modified:
+//
+//    18 February 2019
+//
+//  Purpose:
+//
+//    printResult is the function file to obtain the derivative vector at a given time.
+//
+//  Inputs:
+//
+//    state: State result vector obtained from rk4 function
+//
+//  Output:
+//
+//    Returns void
+//
+//******************************************************************************
   for (int i = 0; i < N+1; ++i) {
-    for (int j = 0; j < 3; ++j) {
+    for (int j = 0; j < S; ++j) {
+			// Print all the state vector values at all input times
       cout << "state[" <<i<<"][" <<j <<"] = " << state[i][j] << " ";
     }
     cout << endl;
   }
-
 }
+
