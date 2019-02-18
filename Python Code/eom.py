fd5e818859b9e01b1fb7c4934f22966bcf92a2df
kshitijmall <horizonkind@gmail.com>
2019-02-18 16:07:07 -0500

    Commented the code

--- Python Code/eom.py
+++ Python Code/eom.py
@@ -1,30 +1,29 @@
 def eom (t,x):
-#
-## eom includes all the dynamic constraints in the problem
-#
-#  Discussion:
-#
-#  Licensing: This code is distributed under the GNU LGPL license.
+################################################################################
+## eom calculates the state derivative vectors at the given time and states
 #
 #  Author: Kshitij Mall
 #
-#  Modified: 13 February 2019
+#  Modified: 18 February 2019
 #  Parameters:
 #
-#    Input, real T0, the current time.
+#    Input: double t, the given time.
 #
-#    Input, integer M, the spatial dimension.
+#           double x, the given state vector.
 #
-#    Output, real U(M), the fourth-order Runge-Kutta solution
-#    estimate at time T0+DT.
+#    Output: double xdot: the state derivative vector
 #
+################################################################################
+# Import the necessary packages
     import numpy as np
+    # Inputs to calculate the state derivatives
     g = 9.80665
     re = 6378000
     B = 157
     rho0 = 1.225
     H = 7200
-    xdot = [x[1]*np.sin(x[2]),\
-           -rho0*np.exp(-x[0]/H)*x[1]**2/(2*B)-g*np.sin(x[2]),\
-           (x[1]/(re+x[0]) - g/x[1])*np.cos(x[2])]
+    # Calculate the state derivative vector
+    xdot = [x[1]*np.sin(x[2]),\ # hdot
+           -rho0*np.exp(-x[0]/H)*x[1]**2/(2*B)-g*np.sin(x[2]),\ # vdot
+           (x[1]/(re+x[0]) - g/x[1])*np.cos(x[2])] # fpa dot
     return xdot
