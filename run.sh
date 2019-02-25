6bbb874cfc45f3bab971e1a2dc044b7a39f7fdce
kshitijmall <horizonkind@gmail.com>
2019-02-24 22:35:28 -0500

    Made minor changes to the comments.

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
-# This shell code executes the main python module.
-# It reads the input in the main file and then calls the rk4 and eom files.
-# It obtains the energy plot based on the datet obtained for h and v.
+# This shell code executes the main python module, main cython module and the cpp file.
 cd Python\ Code/
 python ./main.py
 cd ..
